707640a008fe1435007dcb0b63c1b0aaf39ce483
Jon Urquieta <jonathan@aikido.dev>
2026-03-18 09:31:29 -0700

    used aikido auto fix terraform aws ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -12,11 +12,16 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=****MAAA
+export AWS_SECRET_ACCESS_KEY=****AKEY
 export AWS_DEFAULT_REGION=us-west-2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens = "required"
+    http_put_response_hop_limit = 1
+  }
   tags = merge({
     Name = "${local.resource_prefix.value}-ec2"
     }, {
